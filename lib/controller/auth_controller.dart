c4a1a20167de660d30beb383717cad47ab23973b
Bibash Karki <111418772+Master-Bibash@users.noreply.github.com>
2023-11-11 09:13:01 +0545

    Update auth_controller.dart

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -50,25 +50,29 @@ class AuthController extends GetxController {
   }
   
 
-  static void createUser(String email, String password, BuildContext context) async {
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password);
+static void createUser(String email, String password, BuildContext context) async {
+  try {
+    await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    reusableSnackbar.showSnackBar(context, "Account created");
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+  } on FirebaseAuthException catch (e) {
+    print("Firebase Auth Error: ${e.code}, ${e.message}");
+    if (e.code == 'weak-password') {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Account created"),
+        content: const Text("Password is too weak. It should be at least 6 characters."),
       ));
-      Navigator.pushReplacementNamed(context, "/loginPage");
-    } on FirebaseAuthException catch (e) {
-      print("Firebase Auth Error: ${e.code}, ${e.message}");
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: Text(e.message.toString()),
       ));
-    } catch (e) {
-      print("An error occurred: $e");
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("An error occurred: $e"),
-      ));
     }
+  } catch (e) {
+    print("An error occurred: $e");
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text("An error occurred: $e"),
+    ));
   }
+}
 
   static void sendResetLink(String email, BuildContext context) async {
     try {
